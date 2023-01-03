bec638f99059d932090ca3e3d62ad2680c48e004
Jean Malm <60971564+jean-malm-mdh@users.noreply.github.com>
2023-01-03 14:47:40 +0100

    [MacOS] Base upload of Avalonia version

---
+++ dva229_lab4_AvaloniaElmish/Calculator.fs
@@ -0,0 +1,62 @@
+﻿
+namespace DVA229_Lab4_AvaloniaElmish
+
+module Calculator =
+    open Avalonia.Controls
+    open Avalonia.FuncUI.DSL
+    open Avalonia.Layout    
+
+    type UserAction = Zero | One | Two | Three
+    type State = int list
+
+    let init : State = []
+
+    let update (msg : UserAction) (state : State) =
+        match msg with
+        | Zero -> 0::state
+        | One -> 1::state
+        | Two -> 2::state
+        | Three -> 3::state
+        | _ -> failwith "TODO: Not implemented yet"
+    
+    let view (state : State) (dispatch) =
+        printfn "%A" state
+        let btnWidth = 70.0
+        let buttonsInColumn = 3.0
+        DockPanel.create [
+            DockPanel.children [
+                TextBlock.create [
+                    TextBlock.dock Dock.Top
+                    TextBlock.fontSize 42.0
+                    TextBlock.verticalAlignment VerticalAlignment.Center
+                    TextBlock.horizontalAlignment HorizontalAlignment.Center
+                    TextBlock.text (sprintf "%A" state)
+                ]
+                WrapPanel.create [
+                    WrapPanel.dock Dock.Bottom
+                    WrapPanel.itemWidth btnWidth
+                    WrapPanel.maxWidth (btnWidth * buttonsInColumn)
+                    WrapPanel.children [
+                        Button.create [
+                            Button.onClick (fun _ -> dispatch One)
+                            Button.content "Add 1"
+                        ]
+                        Button.create [
+                            Button.onClick (fun _ -> dispatch Two)
+                            Button.content "Add 2"
+                        ]
+                        Button.create [
+                            Button.onClick (fun _ -> dispatch Three)
+                            Button.content "Add 3"
+                        ]
+                        Button.create [
+                            Button.width (btnWidth * buttonsInColumn)
+                            Button.onClick (fun _ -> dispatch Zero)
+                            Button.content "Add 0"
+                            Button.horizontalContentAlignment HorizontalAlignment.Center
+                            Button.verticalAlignment VerticalAlignment.Center
+                        ]                
+                    ]
+                ]
+            ]
+        ]
